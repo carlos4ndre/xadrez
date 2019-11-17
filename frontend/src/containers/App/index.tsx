a544fe2728012fbd2f0c7e0ab368ab5f799352cd
Carlos André <carlos.ulrich@gmail.com>
2019-11-16 20:56:03 -0500

    Add typescript

---
+++ frontend/src/containers/App/index.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>Hello Xadrez!</div>
+    )
+  }
+}
+
+export default App
---
+++ frontend/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from 'containers/App'
+import 'semantic-ui-css/semantic.min.css'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
