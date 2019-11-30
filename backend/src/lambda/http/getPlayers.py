47076315d28a948bce014e5b83e82f65812e0222
Carlos André <carlos.ulrich@gmail.com>
2019-11-30 14:59:37 -0500

    Get players from dynamoDB

--- backend/src/lambda/http/getPlayers.py
+++ backend/src/lambda/http/getPlayers.py
@@ -1,23 +1,32 @@
+import os
 import logging
+import boto3
 import json
 
 logger = logging.getLogger(__name__)
 
+dynamodb = boto3.resource("dynamodb")
+
 
 def handler(event, context):
-    logger.info("Get players")
-    data = {
-        "players": [
-            {'id': '1', 'name': 'toze', 'email': 'toze@test.com', 'nickname': 'tomatos', 'picture': ''},
-            {'id': '2', 'name': 'batman', 'email': 'batman@test.com', 'nickname': 'bruce', 'picture': ''},
-            {'id': '3', 'name': 'luffy', 'email': 'luffy@test.com', 'nickname': 'muggywara', 'picture': ''}
-        ]
-    }
+    try:
+        logger.info("Get Players")
+        table = dynamodb.Table(os.environ["PLAYERS_TABLE"])
+        response = table.scan()
+        players = response['Items']
+        while 'LastEvaluatedKey' in response:
+            response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+            players.extend(response['Items'])
+    except Exception as e:
+        logger.error(e)
+        return {"statusCode": 500, "body": "Connect failed"}
 
     return {
         "statusCode": 200,
         "headers": {
             "Access-Control-Allow-Origin": "*"
         },
-        "body": json.dumps(data)
+        "body": json.dumps({
+            "players": players
+        })
     }
