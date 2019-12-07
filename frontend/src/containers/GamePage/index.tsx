63cba851c929e680682ca6ce24ca8534f8f8a1f5
Carlos André <carlos.ulrich@gmail.com>
2019-12-07 01:03:50 -0500

    Add dummy chessboard

--- frontend/src/containers/GamePage/index.tsx
+++ frontend/src/containers/GamePage/index.tsx
@@ -2,11 +2,26 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { AppState } from 'types/state'
 import { GameProps } from 'types/props'
+import { Grid, Header } from 'semantic-ui-react'
+import ChessBoard from 'containers/ChessBoard'
 
-class GamePage extends Component<GameProps, AppState> {
+class GamePage extends Component<any, any> {
   render() {
     return (
-      <div>Game Page</div>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column>
+            <Header as='h1' color='black' textAlign='center'>Game</Header>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row centered columns={3}>
+          <Grid.Column>
+            <ChessBoard />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row centered columns={3}>
+        </Grid.Row>
+      </Grid>
     )
   }
 }
