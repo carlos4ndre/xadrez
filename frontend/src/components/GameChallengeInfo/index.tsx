6575f0baa69d706e327bca6c9a2da3155ab4a1e3
Carlos André <carlos.ulrich@gmail.com>
2019-12-16 16:58:11 -0500

    Display time in HH:mm format

--- frontend/src/components/GameChallengeInfo/index.tsx
+++ frontend/src/components/GameChallengeInfo/index.tsx
@@ -3,6 +3,14 @@ import { List } from 'semantic-ui-react'
 import { GameChallengeInfoProps } from 'types/props'
 import PlayerIcon from 'components/PlayerIcon'
 
+function displayTime(timeInSeconds: number) {
+    const pad = function(num: number, size: number) { return ('000' + num).slice(size * -1) }
+    const minutes = Math.floor(timeInSeconds / 60 % 60)
+    const seconds = Math.floor(timeInSeconds - minutes * 60)
+
+    return pad(minutes, 2) + ':' + pad(seconds, 2)
+}
+
 const GameChallengeInfo: React.FunctionComponent<GameChallengeInfoProps> = (props) => (
   <List>
     <List.Item>
@@ -14,7 +22,7 @@ const GameChallengeInfo: React.FunctionComponent<GameChallengeInfoProps> = (prop
     </List.Item>
     <List.Item>
       <List.Icon name='clock outline' />
-      <List.Content>{props.game.time === 0 ? 'No time' : props.game.time}</List.Content>
+      <List.Content>{props.game.time === 0 ? 'No time' : displayTime(props.game.time / 1000) }</List.Content>
     </List.Item>
   </List>
 )
